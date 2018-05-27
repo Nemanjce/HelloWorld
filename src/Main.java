3213cf1085b6947ab49b7e7c16af2a0f2701f292
Nemanja <nemanjce@yahoo.com>
2018-05-27 19:42:02 +0200

    gramaticke,kozmeticke izmene

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ public class Main {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello World!");
+        System.out.println("Hello, World!");
+        
         System.out.println("Nemanja");
+        System.out.println("https://github.com/Nemanjce/HelloWorld");
         
         
     }
